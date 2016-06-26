76f7f9baecb0394f8554f47f4374c025e0d5d0c7
Ankit <Ankit>
2016-06-27 01:00:51 +0530

    first commit

---
+++ mem-size.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+#include <libvmi/libvmi.h>
+
+int main (int argc, char **argv)
+{
+    vmi_instance_t vmi;
+    unsigned char *memory = NULL;
+    uint32_t offset;
+    addr_t list_head = 0, next_list_entry = 0;
+    addr_t current_process = 0;
+    addr_t tmp_next = 0;
+    char *procname = NULL;
+    vmi_pid_t pid = 0;
+    unsigned long tasks_offset = 0, pid_offset = 0, name_offset = 0;
+    status_t status;
+
+    /* this is the VM or file that we are looking at */
+    if (argc != 2) {
+        printf("Usage: %s <vmname>\n", argv[0]);
+        return 1;
+    } // if
+
+    char *name = argv[1];
+
+    /* initialize the libvmi library */
+    if (vmi_init(&vmi, VMI_AUTO | VMI_INIT_COMPLETE, name) == VMI_FAILURE) {
+        printf("Failed to init LibVMI library.\n");
+        return 1;
+    }
+
+    /* init the offset values */
+    
+    /* walk the task list */
+   
+	uint64_t s= vmi_get_memsize (vmi);
+	printf("\nMax Size Of VM is: %"PRIu64" bytes.\n",s);
+	error_exit:
+    /* resume the vm */
+    vmi_resume_vm(vmi);
+
+    /* cleanup any memory associated with the LibVMI instance */
+    vmi_destroy(vmi);
+
+    return 0;
+}
